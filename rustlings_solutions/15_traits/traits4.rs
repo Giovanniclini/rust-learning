78be0f433f50949632ecde881f9bef16aa53e3e6
Giovanni Clini <gioviclini@gmail.com>
2025-07-19 17:15:50 +0200

    Added chapter 15 of rustlings traits

---
+++ rustlings_solutions/15_traits/traits4.rs
@@ -0,0 +1,44 @@
+trait Licensed {
+    fn licensing_info(&self) -> String {
+        "Default license".to_string()
+    }
+}
+
+struct SomeSoftware;
+struct OtherSoftware;
+
+impl Licensed for SomeSoftware {}
+impl Licensed for OtherSoftware {}
+
+// TODO: Fix the compiler error by only changing the signature of this function.
+// The compiler knows that the trait Licensed will always return a String
+// So it knows that the result of licensing_info are comparable and it will compile!
+fn compare_license_types<T,U>(
+    software1: T, 
+    software2: U
+) -> bool 
+where 
+    T: Licensed,
+    U: Licensed
+{
+    software1.licensing_info() == software2.licensing_info()
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn compare_license_information() {
+        assert!(compare_license_types(SomeSoftware, OtherSoftware));
+    }
+
+    #[test]
+    fn compare_license_information_backwards() {
+        assert!(compare_license_types(OtherSoftware, SomeSoftware));
+    }
+}
