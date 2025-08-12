447ce272ffec8cf4498154f25d8013bf735edf16
Giovanni Clini <gioviclini@gmail.com>
2025-08-12 19:29:57 +0200

    Completed chapter 13 of the rust book

--- the_rust_book/chapter13/src/main.rs
+++ the_rust_book/chapter13/src/main.rs
@@ -42,4 +42,65 @@ fn main() {
         .join()
         .unwrap();
 
+    // The way a closure captures and handles values from the environment affects which traits the closure implements, 
+    // and traits are how functions and structs can specify what kinds of closures they can use. 
+    // Closures will automatically implement one, two, or all three of these Fn traits, in an additive fashion, 
+    // depending on how the closure’s body handles the values:
+
+    // 1. FnOnce applies to closures that can be called once. All closures implement at least this trait because all 
+    // closures can be called. A closure that moves captured values out of its body will only implement FnOnce and none 
+    // of the other Fn traits, because it can only be called once.
+    // 2. FnMut applies to closures that don’t move captured values out of their body, 
+    // but that might mutate the captured values. These closures can be called more than once.
+    // 3. Fn applies to closures that don’t move captured values out of their body and that don’t mutate captured values, 
+    // as well as closures that capture nothing from their environment. 
+    // These closures can be called more than once without mutating their environment, 
+    // which is important in cases such as calling a closure multiple times concurrently.
+
+    // ---------------------------
+
+    // Iterators
+    // e.g. iter()
+    // Iterators are lazy, they have no effect until you call methods that consume the iterator. 
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {val}");
+    }
+
+    // All iterators implement the trait:
+    pub trait Iterator {
+    type Item;
+
+    fn next(&mut self) -> Option<Self::Item>;
+
+    // methods with default implementations elided
+    }
+    // This means that with the iterator we also need to implement a type Item
+    // and the next method will return an Some(Item) until the object we are iterating is empty. 
+    // The Iterator trait has a lot of method you with defult implementstion. You only need to implement the next method. 
+    // sum is a consuming adapters: it consumes the iterator and return the sum of the value in the iterator.
+    // Iterators adapter are method that return another form of the iterator.
+    let v1: Vec<i32> = vec![1, 2, 3];
+
+    // This code produces an error since we are not using the iterator that we get from map
+    v1.iter().map(|x| x + 1); // add .collect() to get a new iterator :)
+
+     // ---------------
+     // An example of closures and iterators
+     fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+        shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+    }
+    // into_iter() takes ownership of the vector transforming the vector into an iterator
+    // filter applies the closure that filters base on size of shoes. (closures that capture the environment)
+    // The closure captures the shoe_size parameter from the environment and compares the value with each shoe’s size, keeping only shoes of the size specified.
+
+    // Note that loop and iterators have similar performances!
+
+
+
+
+
 }
